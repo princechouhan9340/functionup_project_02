b2f17f30eeb9c9639448e88a87d7ad1933afb2dc
princechouhan9340 <102707478+princechouhan9340@users.noreply.github.com>
2022-05-06 11:51:47 +0530

    Update collagecontroller.js

--- src/controller/collagecontroller.js
+++ src/controller/collagecontroller.js
@@ -1,6 +1,7 @@
-const collageModel = require("../model/collageModel")
+const collegeModel = require("../model/collegeModel")
 const internModel = require("../model/internModel")
-const createcollage = async function (req, res) {
+
+const createcollege = async function (req, res) {
     try {
         const data = req.body
 
@@ -12,39 +13,40 @@ const createcollage = async function (req, res) {
             return res.status(400).send({ status: false, Error: "please enter mandatory field" })
         }
 
-        if (typeof (data.name) != "string" || typeof (data.fullName) != "string" || typeof(data.logoLink) != "string") {
+        if (typeof (data.name) != "string" || typeof (data.fullName) != "string" || typeof (data.logoLink) != "string") {
             return res.status(400).send({ status: false, Error: "invalid input" })
         }
 
-
-        const result = await collageModel.create(data)
+        const result = await collegeModel.create(data)
         res.status(201).send({ status: true, data: result })
     } catch (err) {
         res.status(500).status({ status: false, Error: err })
     }
 }
 
-const getcollagedetail = async function (req,res){
-    try{
-        const collagename = req.query
-        if(Object.keys(collagename).length == 0){
-            return res.status(400).send({status:false,Error:"empty request"})
+const getcollegedetail = async function (req, res) {
+    try {
+        const collegename = req.query
+        if (Object.keys(collegename).length == 0) {
+            return res.status(400).send({ status: false, Error: "empty request" })
         }
-        let collage = Object.values(collagename).toLocaleString().trim()
-        console.log(collage)
-        const findcollage = await collageModel.findOne({name:collage}).select({name:1,fullName:1,logoLink:1})
-        const findcollageid = await collageModel.findOne({name:collage})
-        console.log(findcollage)
-        if(!findcollage){
-            return res.status(400).send({status:false,Error:"collage not found"})
+       // collagename={name:iitk}
+        let college = Object.values(collegename).toLocaleString().trim()
+        console.log(college)
+        const findcollege = await collegeModel.findOne({ name: college }).select({ name: 1, fullName: 1, logoLink: 1, _id: 0 })
+        const findcollegeid = await collegeModel.findOne({ name: college })
+        console.log(findcollege)
+        if (!findcollege) {
+            return res.status(400).send({ status: false, Error: "collage not found" })
         }
 
-        const result = await internModel.find({collageId:findcollageid._id}).select({_id:1,name:1,email:1,mobile:1,_id:0})
-        res.status(200).send({status:true,data:findcollage,interns:result})
+        const result = await internModel.find({ collegeId: findcollegeid._id }).select({ _id: 1, name: 1, email: 1, mobile: 1 })
+        res.status(200).send({ status: true, data: findcollege, interns: result })
 
-    }catch(err){
-        res.status(500).send({status:false,Error:err})    }
+    } catch (err) {
+        res.status(500).send({ status: false, Error: err })
+    }
 }
 
-module.exports.createcollage = createcollage
-module.exports.getcollagedetail = getcollagedetail
+module.exports.createcollege = createcollege
+module.exports.getcollegedetail = getcollegedetail
